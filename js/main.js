06b3671e2ce31fe61f9fe9cc2f6b3054e86f18d1
ryangois <ryangoisdev@gmail.com>
2023-05-06 19:33:26 -0300

    importing elements

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 import Controls from "./controls.js"
 import Timer from "./timer.js"
-
-let playButton = document.querySelector(".play")
-let pauseButton = document.querySelector(".pause")
-let stopButton = document.querySelector(".stop")
-let setButton = document.querySelector(".set")
-let soundButton = document.querySelector(".sound")
-let muteButton = document.querySelector(".mute")
-const minutesDisplay = document.querySelector(".minutes")
-const secondsDisplay = document.querySelector(".seconds")
+import {
+    playButton,
+    pauseButton,
+    stopButton,
+    setButton,
+    soundButton,
+    muteButton,
+    minutesDisplay,
+    secondsDisplay
+} from "./elements.js"
 
 const controls = Controls({
     playButton,
@@ -40,7 +41,7 @@ setButton.addEventListener("click", () => {
         timer.reset()
         return
     }
-    
+
     timer.displayUpdate(newMinutes, 0)
     timer.updateMinutes(newMinutes)
 })
