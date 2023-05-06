d450efdf00bf0426d7fb6ec81d651b6ddb52d87c
ryangois <ryangoisdev@gmail.com>
2023-05-06 19:33:09 -0300

    new folder for DOM elements

---
+++ js/elements.js
@@ -0,0 +1,19 @@
+const playButton = document.querySelector(".play")
+const pauseButton = document.querySelector(".pause")
+const stopButton = document.querySelector(".stop")
+const setButton = document.querySelector(".set")
+const soundButton = document.querySelector(".sound")
+const muteButton = document.querySelector(".mute")
+const minutesDisplay = document.querySelector(".minutes")
+const secondsDisplay = document.querySelector(".seconds")
+
+export {
+    playButton,
+    pauseButton,
+    stopButton,
+    setButton,
+    soundButton,
+    muteButton,
+    minutesDisplay,
+    secondsDisplay
+}
